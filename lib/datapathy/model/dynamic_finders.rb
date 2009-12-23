ae3161b52335e0a79c536e6d6bb1df8571af5122
Paul Sadauskas <psadauskas@gmail.com>
2009-12-22 17:08:00 -0700

    Refactor dynamic finders to be a little cleaner

--- lib/datapathy/model/dynamic_finders.rb
+++ lib/datapathy/model/dynamic_finders.rb
@@ -8,37 +8,44 @@ module Datapathy::Model
       def method_missing(method_id, *arguments, &block)
         if match = DynamicFinderMatch.match(method_id)
           if match.finder?
-            self.class_eval %{
-              def self.#{method_id}(*args)
-                find_attributes = Hash[[:#{match.attribute_names.join(',:')}].zip(args)]
-
-                #{match.finder == :first ? "detect(find_attributes)" : "select(find_attributes)"}
-              end
-            }, __FILE__, __LINE__
-            send(method_id, *arguments, &block)
+            define_find_by(method_id, match)
           elsif match.instantiator?
-            self.class_eval %{
-              def self.#{method_id}(*args)
-                if args[0].is_a?(Hash)
-                  attributes = args[0]
-                  find_attributes = attributes.slice(*[:#{match.attribute_names.join(',:')}])
-                end
-
-                record = detect(find_attributes)
-
-                if record.nil?
-                  record = self.new(attributes)
-                  #{'record.save' if match.instantiator == :create}
-                end
-
-                record
-              end
-            }, __FILE__, __LINE__
-            send(method_id, *arguments, &block)
+            define_find_or_create_by(method_id, match)
           end
+          send(method_id, *arguments, &block)
         else
           super
         end
+      end
+
+      def define_find_by(method_id, match)
+        self.class_eval %{
+          def self.#{method_id}(*args)
+            find_attributes = Hash[[:#{match.attribute_names.join(',:')}].zip(args)]
+
+            #{match.finder == :first ? "detect(find_attributes)" : "select(find_attributes)"}
+          end
+        }, __FILE__, __LINE__
+      end
+
+      def define_find_or_create_by(method_id, match)
+        self.class_eval %{
+          def self.#{method_id}(*args)
+            if args[0].is_a?(Hash)
+              attributes = args[0]
+              find_attributes = attributes.slice(*[:#{match.attribute_names.join(',:')}])
+            end
+
+            record = detect(find_attributes)
+
+            if record.nil?
+              record = self.new(attributes)
+              #{'record.save' if match.instantiator == :create}
+            end
+
+            record
+          end
+        }, __FILE__, __LINE__
       end
 
       class DynamicFinderMatch
@@ -57,9 +64,6 @@ module Datapathy::Model
             @finder = :last if $1 == 'last_by'
             @finder = :all if $1 == 'all_by'
             names = $2
-          when /^find_by_([_a-zA-Z]\w*)\!$/
-            @bang = true
-            names = $1
           when /^find_or_(initialize|create)_by_([_a-zA-Z]\w*)$/
             @instantiator = $1 == 'initialize' ? :new : :create
             names = $2
@@ -75,10 +79,6 @@ module Datapathy::Model
 
         def instantiator?
           @finder == :first && !@instantiator.nil?
-        end
-
-        def bang?
-          @bang
         end
 
       end
